5ab47aa199a143b639903b61dfe473fa42efe004
d-kyon <ky0ya38365388@gmail.com>
2019-06-01 07:02:52 +0000

    GUIの向上

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -1,56 +1,8 @@
 class WebhookController < ApplicationController
+  include WebhookHelper
   protect_from_forgery :except => [:callback]
   
-  def confirm(content,messages,event)
-    template= {
-                'type'    => 'confirm',
-                'text'    => content+'は「' + event.message['text'] + ' 」でよろしいでしょうか？
-よろしければ「はい」
-修正したい場合は「修正する」を押してください',
-                'actions' => [
-                                {'type'=>'message', 'label'=>'はい', 'text'=>'はい'},
-                                {'type'=>'message', 'label'=>'修正する', 'text'=>'修正する'}
-                              ]
-              }
-              message = {
-                'type'     => 'template',
-                 'altText'  => content+'は「' + event.message['text'] + ' 」でよろしいでしょうか？
-よろしければ「はい」
-修正したい場合は「修正する」と入力してください',
-                 'template' => template
-              }
-              messages.push(message)
-  end
   
-  def apply(messages,event)
-    template= {
-                'type'    => 'buttons',
-                'thumbnailImageUrl' => 'https://akitennis-it-lesson.info/assets/lp1_img2.jpg',
-                'title'   => 'AKIREVO予約サービス' ,
-                'text'    => 'こんにちは、AKIREVO予約サービスです！申し込みをしたい場合は「申し込む」をタップしてください',
-                'actions' => [
-                                {'type'=>'message', 'label'=>'申し込む', 'text'=>'申し込む'}
-                              ]
-              }
-              message = {
-                'type'     => 'template',
-                 'altText'  => 'こんにちは、AKIREVO予約サービスです！
-申し込みをしたい場合は「申し込み」と入力して下さい',
-                 'template' => template
-              }
-              messages.push(message)
-  end
-  
-  def location(messages)
-    message= {
-                'type'      => 'location',
-                'title'     => 'アクセス',
-                'address'   => 'セントラルパークタワー　ラ・トゥール新宿',
-                'latitude'  => 35.692179,
-                'longitude' => 139.688554
-                }
-                messages.push(message)
-  end
   
   def callback
     body = request.body.read
@@ -69,8 +21,9 @@ class WebhookController < ApplicationController
           messages=[]
           if event.message['text']=='申し込む' && user.nil?
             User.create!(line_id:event['source']['userId'])
-            response='申し込みありがとうございます。
-まずはお名前を漢字で入力してください。'
+            template("申し込みありがとうございます。
+まずはお名前を漢字で入力してください。",messages,event)
+            
           else
             if user.nil? then
               apply(messages,event)
@@ -85,12 +38,12 @@ class WebhookController < ApplicationController
               case event.message['text']
               when 'はい'
                 user.update!(name_is:true)
-                response="それでは次にお名前のふりがなを入力してください"
+                template("それでは次にお名前のふりがなを入力してください",messages,event)
               when '修正する'
                 user.update!(name:nil)
-                response="それでは今一度お名前を漢字で入力してください"
+                template("それでは今一度お名前を漢字で入力してください",messages,event)
               else
-                response="「はい」か「修正する」でお答えください"
+                template("「はい」か「修正する」でお答えください",messages,event)
               end
 
               # ふりがな名前入力の確認
@@ -103,12 +56,12 @@ class WebhookController < ApplicationController
               case event.message['text']
               when 'はい'
                 user.update!(name_kana_is:true)
-                response="それでは次にメールアドレスを入力してください"
+                template("それでは次にメールアドレスを入力してください",messages,event)
               when '修正する'
                 user.update!(name_kana:nil)
-                response="それでは今一度お名前をひらがなで入力してください"
+                template("それでは今一度お名前をひらがなで入力してください",messages,event)
               else
-                response="「はい」か「修正する」でお答えください"
+                template("「はい」か「修正する」でお答えください",messages,event)
               end
 
               # メールアドレス入力の確認
@@ -121,12 +74,12 @@ class WebhookController < ApplicationController
               case event.message['text']
               when 'はい'
                 user.update!(email_is:true)
-                response="それでは次に電話番号を入力してください"
+                template("それでは次に電話番号を入力してください",messages,event)
               when '修正する'
                 user.update!(email:nil)
-                response="それでは今一度メールアドレスを入力してください"
+                template("それでは今一度メールアドレスを入力してください",messages,event)
               else
-                response="「はい」か「修正する」でお答えください"
+                template("「はい」か「修正する」でお答えください",messages,event)
               end
 
               # 電話番号入力の確認
@@ -139,13 +92,13 @@ class WebhookController < ApplicationController
               case event.message['text']
               when 'はい'
                 user.update!(phone_number_is:true)
-                response="それでは次にお持ちでしたら招待コードを入力してください
-お持ちでない場合は「なし」と入力してください"
+                template("それでは次にお持ちでしたら招待コードを入力してください
+お持ちでない場合は「なし」と入力してください",messages,event)
               when '修正する'
                 user.update!(phone_number:nil)
-                response="それでは今一度電話番号を入力してください"
+                template("それでは今一度電話番号を入力してください",messages,event)
               else
-                response="「はい」か「修正する」でお答えください"
+                template("「はい」か「修正する」でお答えください",messages,event)
               end
 
               # 電話番号入力の確認
@@ -158,13 +111,13 @@ class WebhookController < ApplicationController
               case event.message['text']
               when 'はい'
                 user.update!(introduction_code_is:true)
-                response="それでは最後に意気込みを入力してください"
+                template("それでは最後に意気込みを入力してください",messages,event)
               when '修正する'
                 user.update!(introduction_code:nil)
-                response="それでは今一度招待コードを入力してください
-お持ちでない場合は「なし」と入力してください"
+                template("それでは今一度招待コードを入力してください
+お持ちでない場合は「なし」と入力してください",messages,event)
               else
-                response="「はい」か「修正する」でお答えください"
+                template("「はい」か「修正する」でお答えください",messages,event)
               end
 
               # 意気込み入力の確認
@@ -177,41 +130,26 @@ class WebhookController < ApplicationController
               case event.message['text']
               when 'はい'
                 user.update!(self_is:true)
+                summary(messages,user)
                 location(messages)
-              
-                response="以上で申し込みは完了です。上記会場にてご来場お待ちしております。
-==申し込み内容==
-お名前：#{user.name}
-お名前(ふりがな)：#{user.name_kana}
-メールアドレス：#{user.email}
-電話番号：#{user.phone_number}
-招待コード：#{user.introduction_code}
-意気込み：#{user.self}"
               when '修正する'
                 user.update!(self:nil)
-                response="それでは今一度意気込みを入力してください"
+                template("それでは今一度意気込みを入力してください",messages,event)
               else
-                response="「はい」か「修正する」でお答えください"
+                template("「はい」か「修正する」でお答えください",messages,event)
               end
             else
+              summary(messages,user)
               location(messages)
-              response="申し込みは完了しております。上記会場にてご来場お待ちしております。
-==申し込み内容==
-お名前：#{user.name}
-お名前(ふりがな)：#{user.name_kana}
-メールアドレス：#{user.email}
-電話番号：#{user.phone_number}
-招待コード：#{user.introduction_code}
-意気込み：#{user.self}"
             end
           end
-          if response then
-            message = {
-              type: 'text',
-              text: response
-            }
-          messages.push(message)
-          end
+          # if response then
+          #   message = {
+          #     type: 'text',
+          #     text: response
+          #   }
+          # messages.push(message)
+          # end
           client.reply_message(event['replyToken'], messages)
           
         else
@@ -225,4 +163,6 @@ class WebhookController < ApplicationController
     }
     "OK"
   end
+  
+
 end
--- app/helpers/webhook_helper.rb
+++ app/helpers/webhook_helper.rb
@@ -1,2 +1,380 @@
 module WebhookHelper
+    def confirm(content,messages,event)
+        contents = {
+                  "type": "bubble",
+                  "hero": {
+                    "type": "image",
+                    "url": "https://akitennis-it-lesson.info/assets/lp1_img2.jpg",
+                    "size": "full",
+                    "aspectRatio": "20:13",
+                    "aspectMode": "cover",
+                    "action": {
+                      "type": "uri",
+                      "uri": "https://akirevo.com/"
+                    }
+                  },
+                  "body": {
+                    "type": "box",
+                    "layout": "vertical",
+                    "spacing": "md",
+                    "contents": [
+                      {
+                        "type": "text",
+                        "text": "AKIREVO予約サービス",
+                        "wrap": true,
+                        "weight": "bold",
+                        "gravity": "center",
+                        "size": "xl"
+                      },
+                      {
+                        "type": "box",
+                        "layout": "vertical",
+                        "margin": "xs",
+                        "contents": [
+                          {
+                            "type": "spacer"
+                          },
+                          {
+                            "type": "text",
+                            "text": content + "は「" + event.message['text'] + "」でよろしいでしょうか？よろしければ「はい」修正したい場合は「修正する」を押してください",
+                            "color": "#000000",
+                            "wrap": true,
+                            "margin": "xs",
+                            "size": "md"
+                          }
+                        ]
+                      }
+                    ]
+                  },
+                  "footer": {
+                    "type": "box",
+                    "layout": "vertical",
+                    "spacing": "sm",
+                    "contents": [
+                      {
+                        "type": "button",
+                        "style": "link",
+                        "height": "sm",
+                        "action": {
+                          "type": "message",
+                          "label": "はい",
+                          "text": "はい"
+                        }
+                      },
+                      {
+                        "type": "button",
+                        "style": "link",
+                        "height": "sm",
+                        "action": {
+                          "type": "message",
+                          "label": "修正する",
+                          "text": "修正する"
+                        }
+                      },
+                      {
+                        "type": "spacer",
+                        "size": "sm"
+                      }
+                    ],
+                    "flex": 0
+                  }
+                }
+              message = {
+                'type'     => 'flex',
+                'altText'  => content+'は「' + event.message['text'] + ' 」でよろしいでしょうか？
+よろしければ「はい」
+修正したい場合は「修正する」と入力してください',
+                'contents' => contents
+                        }
+              messages.push(message)
+    end
+  
+    def apply(messages,event)
+        template= {
+                    'type'    => 'buttons',
+                    'thumbnailImageUrl' => 'https://akitennis-it-lesson.info/assets/lp1_img2.jpg',
+                    'title'   => 'AKIREVO予約サービス' ,
+                    'text'    => 'こんにちは、AKIREVO予約サービスです！申し込みをしたい場合は「申し込む」をタップしてください',
+                    'actions' => [
+                                    {'type'=>'message', 'label'=>'申し込む', 'text'=>'申し込む'}
+                                  ]
+                  }
+                  message = {
+                    'type'     => 'template',
+                     'altText'  => 'こんにちは、AKIREVO予約サービスです！
+    申し込みをしたい場合は「申し込む」と入力して下さい',
+                     'template' => template
+                  }
+                  messages.push(message)
+    end
+      
+    def location(messages)
+        message= {
+                    'type'      => 'location',
+                    'title'     => 'アクセス',
+                    'address'   => 'セントラルパークタワー　ラ・トゥール新宿',
+                    'latitude'  => 35.692179,
+                    'longitude' => 139.688554
+                    }
+                    messages.push(message)
+    end
+  
+    def summary(messages,user)
+        contents = {
+                    "type": "bubble",
+                    "hero": {
+                      "type": "image",
+                      "url": "https://akitennis-it-lesson.info/assets/lp1_img2.jpg",
+                      "size": "full",
+                      "aspectRatio": "20:13",
+                      "aspectMode": "cover",
+                      "action": {
+                        "type": "uri",
+                        "uri": "https://akirevo.com/"
+                      }
+                    },
+                    "body": {
+                      "type": "box",
+                      "layout": "vertical",
+                      "spacing": "md",
+                      "contents": [
+                        {
+                          "type": "text",
+                          "text": "AKIREVO予約サービス",
+                          "wrap": true,
+                          "weight": "bold",
+                          "gravity": "center",
+                          "size": "xl"
+                        },
+                        {
+                          "type": "box",
+                          "layout": "vertical",
+                          "margin": "lg",
+                          "spacing": "sm",
+                          "contents": [
+                            {
+                              "type": "box",
+                              "layout": "baseline",
+                              "spacing": "sm",
+                              "contents": [
+                                {
+                                  "type": "text",
+                                  "text": "名前",
+                                  "color": "#aaaaaa",
+                                  "size": "sm",
+                                  "flex": 2
+                                },
+                                {
+                                  "type": "text",
+                                  "text": user.name,
+                                  "wrap": true,
+                                  "size": "sm",
+                                  "color": "#666666",
+                                  "flex": 4
+                                }
+                              ]
+                            },
+                            {
+                              "type": "box",
+                              "layout": "baseline",
+                              "spacing": "sm",
+                              "contents": [
+                                {
+                                  "type": "text",
+                                  "text": "ふりがな",
+                                  "color": "#aaaaaa",
+                                  "size": "sm",
+                                  "flex": 2
+                                },
+                                {
+                                  "type": "text",
+                                  "text": user.name_kana,
+                                  "wrap": true,
+                                  "color": "#666666",
+                                  "size": "sm",
+                                  "flex": 4
+                                }
+                              ]
+                            },
+                            {
+                              "type": "box",
+                              "layout": "baseline",
+                              "spacing": "sm",
+                              "contents": [
+                                {
+                                  "type": "text",
+                                  "text": "e-mail",
+                                  "color": "#aaaaaa",
+                                  "size": "sm",
+                                  "flex": 2
+                                },
+                                {
+                                  "type": "text",
+                                  "text": user.email,
+                                  "wrap": true,
+                                  "color": "#666666",
+                                  "size": "sm",
+                                  "flex": 4
+                                }
+                              ]
+                            },
+                            {
+                              "type": "box",
+                              "layout": "baseline",
+                              "spacing": "sm",
+                              "contents": [
+                                {
+                                  "type": "text",
+                                  "text": "電話番号",
+                                  "color": "#aaaaaa",
+                                  "size": "sm",
+                                  "flex": 2
+                                },
+                                {
+                                  "type": "text",
+                                  "text": user.phone_number,
+                                  "wrap": true,
+                                  "color": "#666666",
+                                  "size": "sm",
+                                  "flex": 4
+                                }
+                              ]
+                            },
+                            {
+                              "type": "box",
+                              "layout": "baseline",
+                              "spacing": "sm",
+                              "contents": [
+                                {
+                                  "type": "text",
+                                  "text": "招待コード",
+                                  "color": "#aaaaaa",
+                                  "size": "sm",
+                                  "flex": 2
+                                },
+                                {
+                                  "type": "text",
+                                  "text": user.introduction_code,
+                                  "wrap": true,
+                                  "color": "#666666",
+                                  "size": "sm",
+                                  "flex": 4
+                                }
+                              ]
+                            },
+                            {
+                              "type": "box",
+                              "layout": "baseline",
+                              "spacing": "sm",
+                              "contents": [
+                                {
+                                  "type": "text",
+                                  "text": "意気込み",
+                                  "color": "#aaaaaa",
+                                  "size": "sm",
+                                  "flex": 2
+                                },
+                                {
+                                  "type": "text",
+                                  "text": user.self,
+                                  "wrap": true,
+                                  "color": "#666666",
+                                  "size": "sm",
+                                  "flex": 4
+                                }
+                              ]
+                            }
+                          ]
+                        },
+                        {
+                          "type": "box",
+                          "layout": "vertical",
+                          "margin": "xxl",
+                          "contents": [
+                            {
+                              "type": "spacer"
+                            },
+                            {
+                              "type": "text",
+                              "text": "以上で申し込みは完了です。下記の場所にてご来場お待ちしております",
+                              "color": "#aaaaaa",
+                              "wrap": true,
+                              "margin": "xxl",
+                              "size": "xs"
+                            }
+                          ]
+                        }
+                      ]
+                    }
+                  }
+        altText="以上で申し込みは完了です。ご連絡をお待ちください。
+お名前：#{user.name}
+お名前(ふりがな)：#{user.name_kana}
+メールアドレス：#{user.email}
+電話番号：#{user.phone_number}
+招待コード：#{user.introduction_code}
+意気込み：#{user.self}"
+        message= {
+                  "type": "flex",
+                  "altText": altText,
+                  "contents": contents
+                  }
+        messages.push(message)
+    end
+    
+    def template(content,messages,event)
+        contents =  {
+                          "type": "bubble",
+                          "hero": {
+                            "type": "image",
+                            "url": "https://akitennis-it-lesson.info/assets/lp1_img2.jpg",
+                            "size": "full",
+                            "aspectRatio": "20:13",
+                            "aspectMode": "cover",
+                            "action": {
+                              "type": "uri",
+                              "uri": "https://akirevo.com/"
+                            }
+                          },
+                          "body": {
+                            "type": "box",
+                            "layout": "vertical",
+                            "spacing": "md",
+                            "contents": [
+                              {
+                                "type": "text",
+                                "text": "AKIREVO予約サービス",
+                                "wrap": true,
+                                "weight": "bold",
+                                "gravity": "center",
+                                "size": "xl"
+                              },
+                              {
+                                "type": "box",
+                                "layout": "vertical",
+                                "margin": "xs",
+                                "contents": [
+                                  {
+                                    "type": "spacer"
+                                  },
+                                  {
+                                    "type": "text",
+                                    "text": content,
+                                    "color": "#000000",
+                                    "wrap": true,
+                                    "margin": "xs",
+                                    "size": "md"
+                                  }
+                                ]
+                              }
+                            ]
+                        }
+                    }
+        message= {
+                  "type": "flex",
+                  "altText": content,
+                  "contents": contents
+                  }
+        messages.push(message)
+    end
 end
